4be9ce0bea6b5275d224b83027077642634d6fc1
withdirection <christopher@withdirection.co>
2026-05-17 11:11:52 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
@@ -33,4 +34,9 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ["**/*.svg", "**/*.csv"],
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 });
